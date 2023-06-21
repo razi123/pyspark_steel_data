17626262c57808e0c4bc13c40b8fffc4831ffac4
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-06-21 14:47:10 +0200

    challenge 1 solution completed

--- src/main.py
+++ src/main.py
@@ -22,8 +22,38 @@ if __name__ == "__main__":
     df_ch1_sales = spark.read.format('csv').option('header', 'True').load(dataset_sales)
     df_ch1_salesmans = spark.read.format('csv').option('header', 'True').load(dataset_salespersons)
 
-    # answers 1 : Details of all cars sold in 2022
     df_answer_1 = src_ch1.question1(df_ch1_cars, df_ch1_sales)
+    df_answer_2 = src_ch1.question2(df_ch1_sales, df_ch1_salesmans)
+    df_answer_3 = src_ch1.question3(df_ch1_cars, df_ch1_sales, df_ch1_salesmans)
+    df_answer_4 = src_ch1.question4(df_ch1_cars, df_ch1_sales, df_ch1_salesmans)
+    df_answer_5 = src_ch1.question5(df_ch1_cars, df_ch1_sales)
+    df_answer_6 = src_ch1.question6(df_ch1_cars, df_ch1_sales, df_ch1_salesmans)
+    df_answer_7 = src_ch1.question7(df_ch1_cars, df_ch1_sales)
+    df_answer_8 = src_ch1.question8(df_ch1_cars, df_ch1_sales)
+    df_answer_9 = src_ch1.question9(df_ch1_sales, df_ch1_salesmans)
+    df_answer_10 = src_ch1.question10(df_ch1_cars, df_ch1_sales, df_ch1_salesmans)
+
+    df_answer_1.show()
+    df_answer_2.show()
+    df_answer_3.show()
+    df_answer_4.show()
+    df_answer_5.show()
+
+    df_answer_6.show()
+    df_answer_7.show()
+    df_answer_8.show()
+    df_answer_9.show()
+    df_answer_10.show()
+
+
+
+
+
+
+
+
+
+
 
 
 
